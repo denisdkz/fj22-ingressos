f94b3c78cfb5794cc0d681e4de7ac7320ec0a4dd
Denis <Denis@caelum.com>
2018-02-28 19:32:03 -0300

    Corrigindo a Sessao

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -26,22 +26,25 @@ public class SessaoController {
 	@Autowired
 	private SessaoDao sessaoDao;
 
-@GetMapping("/admin/sessao")
-public ModelAndView form(@RequestParam("salaId")Integer salaId, SessaoForm form) {
-		
-	ModelAndView modelAndView = new ModelAndView("sessao/sessao");
-	
-modelAndView.addObject("sala", salaDao.findOne(salaId));
-modelAndView.addObject("filmes", filmeDao.findAll());
-	return modelAndView;
-}
+	@GetMapping("/admin/sessao")
+	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form) {
+
+		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+
+		form.setSalaId(salaId);
+
+		modelAndView.addObject("sala", salaDao.findOne(salaId));
+		modelAndView.addObject("filmes", filmeDao.findAll());
+		modelAndView.addObject("form", form);
+		return modelAndView;
+	}
 
 	@PostMapping(value = "/admin/sessao")
 	@Transactional
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
 
 		if (result.hasErrors())
-			return form (form.getSalaId(), form);
+			return form(form.getSalaId(), form);
 
 		ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
 
