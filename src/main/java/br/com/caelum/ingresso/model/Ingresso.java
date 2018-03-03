835f357c4c6757b64327673722284b17b4cf3c7e
Denis <Denis@caelum.com>
2018-03-02 22:20:29 -0300

    Criando o carrinho de compras

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -2,21 +2,41 @@ package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
 
-import br.com.caelum.ingresso.model.descontos.Desconto;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Ingresso {
+	
+	 @Id
+     @GeneratedValue
+     private Integer id;
+	 
+	 @ManyToOne
 	private Sessao sessao;
+	 
     private BigDecimal preco;
-
+    
+    @ManyToOne
+    private Lugar lugar;
+    
+    @Enumerated(EnumType.STRING)
+    private TipoDeIngresso tipoDeIngresso;
     /**
     * @deprecated hibernate only
     */
     public Ingresso(){
 
     }
-    public Ingresso(Sessao sessao, Desconto tipoDeDesconto) {
+    public Ingresso(Sessao sessao, Lugar lugar, TipoDeIngresso tipoDeIngresso) {
         this.sessao = sessao;
-        this.preco = tipoDeDesconto.aplicarDescontoSobre(sessao.getPreco());
+        this.tipoDeIngresso = tipoDeIngresso;
+        this.preco = this.tipoDeIngresso.aplicaDesconto(sessao.getPreco());
+        this.lugar = lugar;
     }
 
     public Sessao getSessao() {
@@ -26,5 +46,15 @@ public class Ingresso {
     public BigDecimal getPreco() {
         return preco;
     }
+	public Lugar getLugar() {
+		return lugar;
+	
+	}
+	public TipoDeIngresso getTipoDeIngresso() {
+		return tipoDeIngresso;
+	}
+	public void setTipoDeIngresso(TipoDeIngresso tipoDeIngresso) {
+		this.tipoDeIngresso = tipoDeIngresso;
+	}
 
 }
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,13 +2,19 @@ package br.com.caelum.ingresso.model;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+
 
 @Entity
 public class Sessao {
@@ -37,6 +43,13 @@ public class Sessao {
         this.sala = sala;
         this.preco = sala.getPreco().add(filme.getPreco());
     }
+    @OneToMany(mappedBy = "sessao", fetch = FetchType.EAGER)
+    private Set<Ingresso> ingressos = new HashSet<>();
+
+    public boolean isDisponivel(Lugar lugarSelecionado) {
+        return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
+    }
+    
 	public Integer getId() {
 		return id;
 	}
